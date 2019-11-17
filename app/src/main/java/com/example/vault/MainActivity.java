e8c62b2492d3181884eda6d06ed07b5649686e3c
bsgriggs <usbeanie@gmail.com>
2019-11-17 15:00:46 -0500

    added generate and password list fragments

--- app/src/main/java/com/example/vault/MainActivity.java
+++ app/src/main/java/com/example/vault/MainActivity.java
@@ -1,45 +1,111 @@
 package com.example.vault;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
-import androidx.core.content.res.ResourcesCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Intent;
-import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle(getString(R.string.homeTitle));
-//        getActionBar().setIcon(R.drawable.home);
 
-        findViewById(R.id.txtNew).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, NewPassword.class));
-            }
-        });
-        findViewById(R.id.txtGenerate).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-        });
-        findViewById(R.id.txtList).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-            }
-        });
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
     }
 
+    @Override
+    protected void onResume() {
+        updateFragment("nav_home");
+        super.onResume();
+    }
+
+    public void updateFragment(String key){
+        switch (key){
+            case "nav_home":
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new home()).commit();
+                toolbar.setTitle("Vault Home");
+                break;
+            case "nav_new":
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new New_Password()).commit();
+                toolbar.setTitle("New Password");
+                break;
+            case "nav_gen":
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new generate()).commit();
+                toolbar.setTitle("Generate");
+                break;
+            case "nav_list":
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new passwords()).commit();
+                toolbar.setTitle("Passwords");
+                break;
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_home:
+                updateFragment("nav_home");
+                break;
+            case R.id.nav_new:
+                updateFragment("nav_new");
+                break;
+            case R.id.nav_gen:
+                updateFragment("nav_gen");
+                break;
+            case R.id.nav_list:
+                updateFragment("nav_list");
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            //TODO - make undo fragment instead of close app
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
---
+++ app/src/main/java/com/example/vault/passwords.java
@@ -0,0 +1,22 @@
+package com.example.vault;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class passwords extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_passwords,container,false);
+
+        return rootView;
+    }
+}
