2ccc38a9f64ae063953b3143c30c63d7900e29b5
bsgriggs <usbeanie@gmail.com>
2019-11-16 23:25:42 -0500

    UI Structure
    added home and new password activities
    TODO:
    -generate password
    -passwords list. 
    -add drawable icons to action bar for each activity
    -add left sliding menu for navigation

--- app/src/main/java/com/example/vault/MainActivity.java
+++ app/src/main/java/com/example/vault/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.vault;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +21,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(getString(R.string.homeTitle));
+//        getActionBar().setIcon(R.drawable.home);
+
+        findViewById(R.id.txtNew).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, NewPassword.class));
+            }
+        });
+        findViewById(R.id.txtGenerate).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+        findViewById(R.id.txtList).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
     }
+
 }
